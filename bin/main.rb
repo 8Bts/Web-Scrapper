c4606373d29edb3ce946329226607c3db3543af7
8Bts <mcmizze@yahoo.com>
2020-06-29 17:43:54 +0400

    Add colorize gem, add @ping_color for GameServerClass

--- bin/main.rb
+++ bin/main.rb
@@ -3,6 +3,11 @@
 require_relative '../lib/scraper.rb'
 require_relative '../lib/game_server.rb'
 
-servers = Scraper.get_servers
+again = true
 
+while again do
+servers = Scraper.get_servers.sort_by! { |server| server.ping }
 servers.each { |server| puts server.to_s }
+print 'Type \'next\' if you want to load more: '
+again = false unless gets.chomp.upcase == 'NEXT'
+end
