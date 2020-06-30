208bd413c855c71f8fdb60eb778b276453a0ad6a
8Bts <mcmizze@yahoo.com>
2020-06-30 11:41:12 +0400

    Start test with Scraper class

--- bin/main.rb
+++ bin/main.rb
@@ -5,8 +5,8 @@ require_relative '../lib/game_server.rb'
 
 again = true
 
-while again do
-servers = Scraper.get_servers.sort_by! { |server| server.ping }
+while again
+servers = Scraper.get_servers.sort! { |a, b| b.ping <=> a.ping }
 servers.each { |server| puts server.to_s }
 print 'Type \'next\' if you want to load more: '
 again = false unless gets.chomp.upcase == 'NEXT'
