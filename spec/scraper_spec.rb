ff4a5dae0cbef04986b00623b6072f1f8d2079fb
8Bts <mcmizze@yahoo.com>
2020-06-30 14:35:47 +0400

    Add testing for Scraper class

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -4,7 +4,12 @@ require_relative '../lib/game_server'
 require_relative '../lib/scraper'
 
 describe Scraper do
-  describe 'Scraper.scrap_list' do
+  describe 'Scraper.parse_servers' do
+
+    it "returns array of GameServer objects which store server information scraped from website" do
+      expect(Scraper.parse_servers.all? { |obj| obj.is_a?(GameServer) && obj.ip_port  }).to be_truthy
+    end
+
   end
 end
 
